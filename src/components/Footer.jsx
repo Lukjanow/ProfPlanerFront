45ebfaa2488a0ebd0ee379848f3a3dd0fee42f48
Lorenz Kehl <mail@lorenzkehl.de>
2024-02-23 10:21:38 +0100

    Implemented the Footer.jsx;

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export function Footer() {
+    return (
+        <footer className={"flex flex-col items-center text-center gap-4 p-3 mt-10"}>
+            <div className={"flex flex-col"}>
+                <h4 className={"text-xl font-bold"}>Team</h4>
+                <span className={"text-xs font-extralight"}>
+                    Artem Gaus, Jannis Bressan, Julius Müller, Lorenz Kehl,<br/>
+                    Lukas Lukjanow, Maik Scherer, Pierre Nitschke, Timo Bühler, Yannick Stock
+                </span>
+            </div>
+            <span className={"text-xs font-extralight"}>
+                © 2024 — ProfPlaner
+            </span>
+        </footer>
+    )
+}
