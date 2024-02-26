07847ddb9e640178f21d4a2ba565f490bfdccc80
Timo_B <timo.buehler01@gmx.de>
2024-02-26 14:20:16 +0100

    Added FilledButton

--- src/components/FilledButton.jsx
+++ src/components/FilledButton.jsx
@@ -3,6 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function FilledButton() {
   return (
-    <div>FilledButton</div>
+    <Button color="primary">
+      <FontAwesomeIcon className={"text-xl"} icon={"plus"} radius={"sm"} />
+      <p className="font-poppins font-normal">Neues Modul</p>
+    </Button>
   );
 }
--- src/pages/BasicDataPage.jsx
+++ src/pages/BasicDataPage.jsx
@@ -1,6 +1,7 @@
 import BasicDataList from "../components/BasicDataMenu";
 import BasicDataTable from "../components/BasicDataTable";
 import { BasicDataButton } from "../components/BasicDataButton";
+import { FilledButton } from "../components/FilledButton";
 
 export default function BasicDataPage() {
   return (
@@ -9,7 +10,7 @@ export default function BasicDataPage() {
       <div className="p-10">
         <div className="flex w-full justify-between">
           <h1 className="font-poppins font-semibold text-[48px]">Stammdaten</h1>
-          <BasicDataButton />
+          <FilledButton />
         </div>
 
         <div className="flex w-full">
