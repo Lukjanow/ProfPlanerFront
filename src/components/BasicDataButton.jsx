43d06c165b9c773d618b4b3d736f82b7c45209c9
Timo_B <timo.buehler01@gmx.de>
2024-02-26 11:42:56 +0100

    added BasicDataList

--- src/components/BasicDataButton.jsx
+++ src/components/BasicDataButton.jsx
@@ -10,3 +10,4 @@ export function BasicDataButton() {
     </Button>
   );
 }
+
--- src/components/BasicDataList.jsx
+++ src/components/BasicDataList.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Listbox, ListboxItem, cn } from "@nextui-org/react";
 
-const BasicDataList = () => {
-  return <div>BasicDataList</div>;
-};
+export default function BasicDataList() {
+  const items = [
+    {
+      key: "module",
+      description: "Module",
+      icon: "pen-to-square",
+    },
+    {
+      key: "room",
+      description: "Räume",
+      icon: "door-closed",
+    },
+    {
+      key: "teacher",
+      description: "Lehrpersonen",
+      icon: "user",
+    },
+    {
+      key: "building",
+      description: "Gebäude",
+      icon: "building",
+    },
+  ];
 
-export default BasicDataList;
+  return (
+    <Listbox
+      variant="flat"
+      aria-label="Listbox menu with descriptions"
+      className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100"
+    >
+      {items.map((item) => (
+        <ListboxItem
+          key={item.key}
+          description={`Erstelle und bearbeite ${item.description}`}
+          startContent={<FontAwesomeIcon icon={item.icon} />}
+        >
+          {item.description}
+        </ListboxItem>
+      ))}
+    </Listbox>
+  );
+}
