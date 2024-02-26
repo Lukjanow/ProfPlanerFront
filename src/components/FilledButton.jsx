95e229672a2e0a89a25767c4e9227e5860738c91
Timo_B <timo.buehler01@gmx.de>
2024-02-26 14:37:37 +0100

    changed border-radius

--- src/components/FilledButton.jsx
+++ src/components/FilledButton.jsx
@@ -4,10 +4,8 @@ import { Button } from "@nextui-org/react";
 
 export function FilledButton({ text, icon, showIcon }) {
   return (
-    <Button color="primary">
-      {showIcon && (
-        <FontAwesomeIcon className={"text-xl"} icon={icon} radius={"sm"} />
-      )}
+    <Button color="primary" radius={"sm"}>
+      {showIcon && <FontAwesomeIcon className={"text-xl"} icon={icon} />}
       <p className="font-poppins font-normal">{text}</p>
     </Button>
   );
