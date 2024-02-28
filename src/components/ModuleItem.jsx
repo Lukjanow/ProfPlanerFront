09403f7daf39bde8b8b8cee4304564607438d1b7
Pierre Nitschke <inf3903@hs-worms.de>
2024-02-28 15:36:06 +0100

    added mockup design

--- src/components/ModuleItem.jsx
+++ src/components/ModuleItem.jsx
@@ -58,7 +58,7 @@ export function ModuleItem({ moduleItemData, dragEvent }) {
 
   //`border-1 border-s-8 w-max border-[${bordercolor}] bg-[${backgroundcolor}] rounded-e-md p-3`}{
   return (
-      <div key={id} id="ModuleItem" className="border-1 border-s-8 w-[320px] rounded-e-md p-3" style={moduleStyle} draggable onDragStart={() => dragEvent(moduleItemData)}>
+      <div key={id} id="ModuleItem" className="border-1 border-s-8 w-[13vw] rounded-e-md p-3" style={moduleStyle} draggable onDragStart={() => dragEvent(moduleItemData)}>
         {setTime()}
         <p className="font-semibold">{title}</p>
         <div className="flex">
--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -5,6 +5,8 @@ import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import { useCallback, useState } from "react";
 import { ModuleItem } from "./ModuleItem";
 import { ModuleBar } from "./ModuleBar";
+import { PageTitle } from "../components/PageTitle";
+import {TimeTableFilter} from "../components/TimeTableFilter";
 import "../styles/components/timeTableEvent.scss";
 
 
@@ -57,39 +59,43 @@ export function TimeTable({moduleItemList}) {
 
   return (
     <>
-      <div className="myCustomHeight">
-          <DnDCalendar
-              className="w-[77vw]"
-              localizer={localizer}
-              events={events}
-              startAccessor="start"
-              endAccessor="end"
-              min={moment("2024-01-01T08:00").toDate()}
-              max={moment("2024-01-01T22:00").toDate()}
-              views={["work_week"]}
-              defaultView="work_week"
-              defaultDate={moment("2024-01-01T00:00").toDate()}
-              toolbar={false}
-              components={{
-                event: CustomEvent
-              }}
-              step={15}
-              timeslots={4}
-              selectable
-              resizable={false}
-              formats={{dayFormat: (date, culture, localizer) => localizer.format(date, "dddd", culture)}}
-              onEventDrop={({ start, end, event }) => {onChangeEventTime(start, end, event.id)}}
-              onDropFromOutside={onDropFromOutside}
-              drilldownView={null}
-          />
+      <div className="flex">
+        <div>
+          <PageTitle text="Lehrplanung"/>
+          <TimeTableFilter></TimeTableFilter>
+          <div className="myCustomHeight">
+              <DnDCalendar
+                  className="w-[78vw]"
+                  localizer={localizer}
+                  events={events}
+                  startAccessor="start"
+                  endAccessor="end"
+                  min={moment("2024-01-01T08:00").toDate()}
+                  max={moment("2024-01-01T22:00").toDate()}
+                  views={["work_week"]}
+                  defaultView="work_week"
+                  defaultDate={moment("2024-01-01T00:00").toDate()}
+                  toolbar={false}
+                  components={{
+                    event: CustomEvent
+                  }}
+                  step={15}
+                  timeslots={4}
+                  selectable
+                  resizable={false}
+                  formats={{dayFormat: (date, culture, localizer) => localizer.format(date, "dddd", culture)}}
+                  onEventDrop={({ start, end, event }) => {onChangeEventTime(start, end, event.id)}}
+                  onDropFromOutside={onDropFromOutside}
+                  drilldownView={null}
+              />
+          </div>
+        </div>
+        <div>
+          <ModuleBar moduleItemList={outsideEvents.map(event => (
+              <ModuleItem moduleItemData={event} dragEvent={setDraggedEvent}/>
+            ))} />
+        </div>
       </div>
-      <div
-            //onDropFromOutside={ backToOverview }
-            >
-              <ModuleBar moduleItemList={outsideEvents.map(event => (
-                  <ModuleItem moduleItemData={event} dragEvent={setDraggedEvent}/>
-                ))} />
-            </div>
     </>
   );
 }
