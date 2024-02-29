146a797a29bf80d1429dd5b2229b15f0beb4047c
Timo_B <timo.buehler01@gmx.de>
2024-02-29 10:27:07 +0100

    Implement dynamic text for 'New' button based on BasicDataMenu selection

--- src/pages/BasicDataPage.jsx
+++ src/pages/BasicDataPage.jsx
@@ -1,38 +1,49 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import BasicDataList from "../components/BasicDataMenu";
+import BasicDataMenu from "../components/BasicDataMenu";
 import BasicDataTable from "../components/BasicDataTable";
 import { FilledButton } from "../components/FilledButton";
 import { OutlinedButton } from "../components/OutlinedButton";
-import { PageTitle } from '../components/PageTitle'
-
+import { PageTitle } from "../components/PageTitle";
 
 export default function BasicDataPage() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+
+  const [selectedItem, setSelectedItem] = useState("module");
+
+  const handleItemClick = (itemKey) => {
+    setSelectedItem(itemKey);
+  };
+
+  const itemKeyToText = {
+    module: t("newModule"),
+    room: t("newRoom"),
+    teacher: t("newLecturer"),
+    building: t("newBuilding"),
+  };
 
   return (
     <>
       <div className="p-10">
         <div className="flex w-full justify-between">
-          <h1 className="font-poppins font-semibold text-[48px]">{t("basicData")}</h1>
+          <h1 className="font-poppins font-semibold text-[48px]">
+            {t("basicData")}
+          </h1>
           <FilledButton
-            text="Neues Modul"
+            text={itemKeyToText[selectedItem]}
             icon="plus"
             showIcon={true}
             onClick={() => {
               console.log("Button wurde geklickt!");
             }}
           />
-
-          <OutlinedButton
-            text="Neues Modul"
-            icon="plus"
-            showIcon={true}
-            color="secondary"
-          />
         </div>
 
         <div className="flex w-full">
-          <BasicDataList />
+          <BasicDataMenu
+            onItemClick={handleItemClick}
+            selectedItem={selectedItem}
+          />
           <BasicDataTable />
         </div>
       </div>
