51513f2a461ad0ac91c7f23e6dc0210c0ea4546b
Timo_B <timo.buehler01@gmx.de>
2024-03-04 10:48:51 +0100

    resolve conflict

--- src/models/moduleModel.js
+++ src/models/moduleModel.js
@@ -1,23 +1,23 @@
 export class ModuleModel {
-    constructor(jsonObj) {
-        this.id = jsonObj.id;
-        this.name = jsonObj.name;
-        this.code = jsonObj.code;
-        this.dozent = jsonObj.dozent;
-        this.room = jsonObj.room;
-        this.studySemester = jsonObj.study_semester;
-        this.duration = jsonObj.duration;
-        this.approximateAttendance = jsonObj.approximate_attendance;
-        this.need = jsonObj.need;
-        this.type = jsonObj.type;
-        this.frequency = jsonObj.frequency;
-        this.selected = jsonObj.selected;
-        this.color = jsonObj.color;
-        this.note = jsonObj.note;
-        this.groups = jsonObj.groups;
-    }
-
-    export class ModuleBasicModel {
+  constructor(jsonObj) {
+    this.id = jsonObj.id;
+    this.name = jsonObj.name;
+    this.code = jsonObj.code;
+    this.dozent = jsonObj.dozent;
+    this.room = jsonObj.room;
+    this.studySemester = jsonObj.study_semester;
+    this.duration = jsonObj.duration;
+    this.approximateAttendance = jsonObj.approximate_attendance;
+    this.need = jsonObj.need;
+    this.type = jsonObj.type;
+    this.frequency = jsonObj.frequency;
+    this.selected = jsonObj.selected;
+    this.color = jsonObj.color;
+    this.note = jsonObj.note;
+    this.groups = jsonObj.groups;
+  }
+}
+export class ModuleBasicModel {
   constructor(jsonObj) {
     this.id = jsonObj.id;
     this.name = jsonObj.name;
