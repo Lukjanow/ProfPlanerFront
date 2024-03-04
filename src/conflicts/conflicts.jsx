12df0dfac97864e2422a1cf93b8dc564c0270800
Pierre Nitschke <inf3903@hs-worms.de>
2024-03-04 13:30:21 +0100

    deleted unused comments

--- src/conflicts/conflicts.jsx
+++ src/conflicts/conflicts.jsx
@@ -1,13 +1,5 @@
 //
 
-// const mod1_name = "EINF"
-// const mod1_start = 15 * 60
-// const mod1_duration = 195
-
-// const mod2_name = "SMK"
-// const mod2_start = 18 * 60
-// const mod2_duration = 195
-
 //function to prove if two modules are overlaping
 //returns boolean value (true = do overlap, false = do not overlap)
 function overlap(mod1, mod2, optional_break=0){
