5478b5191c6577e775c6f1032597333cf3685385
Pierre Nitschke <inf3903@hs-worms.de>
2024-03-04 11:59:57 +0100

    added ignore value to conflict class

--- src/conflicts/Conflict.jsx
+++ src/conflicts/Conflict.jsx
@@ -1,7 +1,8 @@
 class Conflict {
-    constructor(mod1, mod2, index=0) {
+    constructor(mod1, mod2, index=0, ignore=false) {
       this.mod1 = mod1;
       this.mod2 = mod2;
       this.index = index;
+      this.ignore = ignore;
     }
   }
