6f23afcaad8f53ffee770e08825b3234b45dd717
Timo_B <timo.buehler01@gmx.de>
2024-03-05 11:44:27 +0100

    Add translation

--- src/components/NoContent.jsx
+++ src/components/NoContent.jsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, CardBody, Button } from "@nextui-org/react";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const NoContent = () => {
+  const { t } = useTranslation();
+
   const handleRefresh = () => {
     window.location.reload(); // Aktualisiert die Seite
   };
@@ -15,16 +18,15 @@ const NoContent = () => {
           <div className="flex flex-col items-center justify-center p-5">
             <div className="p-2 flex flex-col items-center justify-center">
               <h2 className="text-2xl font-bold mt-4 mb-2 p-5">
-                No Content Available!
+                {t("noContentAvailable")}
               </h2>
               <p className="text-base text-center">
-                You can attempt to add new data using the button above. If data
-                exists but isn't displayed, try refreshing the page.
+                {t("noContentInstruction")}
               </p>
             </div>
 
             <Button color="secondary" onClick={handleRefresh}>
-              Refresh
+              {t("refresh")}
             </Button>
           </div>
         </CardBody>
