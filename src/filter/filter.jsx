dcf1b2e8dc138553a5167ec71c08cfd37e5fa243
Pierre Nitschke <inf3903@hs-worms.de>
2024-03-07 15:44:36 +0100

    added functions for filter

---
+++ src/filter/filter.jsx
@@ -0,0 +1,41 @@
+function filterDozent(dropdownInput){
+    for (const module of module_list) {
+        var visible = false
+        for(const dozent of module.dozent){
+            const dozent_full_name = String(dozent.prename) + " " + String(dozent.lastname)
+            if (dozent_full_name == dropdownInput) {
+                visible = true
+                break;
+            }
+        }
+        module.visible = visible
+    }
+}
+function filterRoom(dropdownInput){
+    for (const module of module_list) {
+        var visible = false
+        for(const room of module.room){
+            const room_name = String(room.roomNumber)
+            if (room_name == dropdownInput) {
+                visible = true
+                break;
+            }
+        }
+        module.visible = visible
+    }
+}
+function filterStudySemester(dropdownInput){
+    for (const module of module_list) {
+        var visible = false
+        for(const study_semester of module.study_semester){
+            const study_semester_name = String(study_semester.name)
+            if (study_semester_name == dropdownInput) {
+                visible = true
+                break;
+            }
+        }
+        module.visible = visible
+    }
+}
+
+export{filterDozent, filterRoom, filterStudySemester}
