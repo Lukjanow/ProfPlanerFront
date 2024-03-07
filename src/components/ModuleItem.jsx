016758bf8652bd7b426b560393ca3653b3c27e7b
Pierre Nitschke <inf3903@hs-worms.de>
2024-03-07 14:03:28 +0100

    changed design of module bar and module item

--- src/components/ModuleItem.jsx
+++ src/components/ModuleItem.jsx
@@ -20,7 +20,7 @@ export function ModuleItem({ moduleItemData, dragEvent }) {
 
   //`border-1 border-s-8 w-max border-[${bordercolor}] bg-[${backgroundcolor}] rounded-e-md p-3`}{
   return (
-      <div className="border-1 border-s-8 w-[13vw] rounded-e-md p-3" style={moduleStyle} draggable onDragStart={() => dragEvent(moduleItemData)}>
+      <div className="border-1 border-s-8 w-[12vw] rounded-e-md p-3" style={moduleStyle} draggable onDragStart={() => dragEvent(moduleItemData)}>
         <p className="font-semibold">{name}</p>
         <div className="flex">
           <span className="flex justify-center items-center justify-self-center w-[30px]"><FontAwesomeIcon icon="fa-solid fa-graduation-cap" /></span><span>{study_semester_string}</span>
