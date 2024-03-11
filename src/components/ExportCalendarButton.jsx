1354ecfadc4247042db76fcc110770d083897aa8
Maik Scherer <inf3934@hs-worms.de>
2024-03-11 17:56:15 +0100

    *export as pdf button (works currently without dynamic values)

--- src/components/ExportCalendarButton.jsx
+++ src/components/ExportCalendarButton.jsx
@@ -1,6 +1,6 @@
 import {Button} from "@nextui-org/react";
 import jsPDF from "jspdf";
-// import html2canvas from "html2canvas";
+import html2canvas from "html2canvas";
 import domtoimage from "dom-to-image";
 
 export function ExportCalendarButton() {
@@ -9,7 +9,8 @@ export function ExportCalendarButton() {
 
         const calendar = document.querySelector('.rbc-calendar');
         const calendarCopy = calendar.cloneNode(true);
-        calendarCopy.style.height = `${calendar.offsetHeight * 2}px`;
+        const calendarHeight = calendar.offsetHeight * 2;
+        calendarCopy.style.height = `${calendarHeight}px`;
 
         const hiddenElements = calendarCopy.querySelectorAll(".hidden");
         hiddenElements.forEach(element => {
@@ -22,19 +23,98 @@ export function ExportCalendarButton() {
         const parentElement = calendar.parentElement;
         parentElement.appendChild(calendarCopy);
 
+        // domtoimage.toPng(calendarCopy)
+        //     .then(dataUrl => {
+        //         const pdf = new jsPDF('l', 'mm');
+        //
+        //         pdf.addImage(dataUrl, 'PNG', 15, 15, 270, 0);
+        //         pdf.save(`${fileName}.pdf`);
+        //
+        //         parentElement.removeChild(calendarCopy);
+        //
+        //     })
+        //     .catch(function (error) {
+        //         console.error('Error exporting calendar:', error);
+        //     });
+
+
+
+
+
+
         domtoimage.toPng(calendarCopy)
             .then(dataUrl => {
                 const pdf = new jsPDF('l', 'mm');
+                const adjustedImageHeight = 500;
+                const totalPages = Math.ceil(adjustedImageHeight / pdf.internal.pageSize.height);
 
-                pdf.addImage(dataUrl, 'PNG', 15, 15, 270, 0);
+                for (let page = 1; page <= totalPages; page++) {
+                    const yOffset = (page - 1) * pdf.internal.pageSize.height;
+                    if (page > 1) {
+                        pdf.addPage();
+                    }
+                    pdf.addImage(dataUrl, 'PNG', 15, 15 - yOffset, 270, adjustedImageHeight);
+                }
+
                 pdf.save(`${fileName}.pdf`);
-
                 parentElement.removeChild(calendarCopy);
 
             })
             .catch(function (error) {
                 console.error('Error exporting calendar:', error);
             });
+
+
+
+
+
+
+
+
+
+
+        // domtoimage.toPng(calendarCopy)
+        //     .then(dataUrl => {
+        //
+        //         const pdf = new jsPDF('l', 'mm');
+        //
+        //         const imgData = dataUrl;
+        //         const imgWidth = 210;
+        //         const pageHeight = 295;
+        //         const imgHeight = calendar.offsetHeight * imgWidth / calendar.offsetWidth;
+        //         let heightLeft = imgHeight;
+        //
+        //         let position = 15;
+        //
+        //         pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        //         heightLeft -= pageHeight;
+        //
+        //         while (heightLeft >= 0) {
+        //             position += heightLeft - imgHeight;
+        //             pdf.addPage();
+        //             pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        //             heightLeft -= pageHeight;
+        //         }
+        //
+        //         pdf.save(`${fileName}.pdf`);
+        //
+        //         parentElement.removeChild(calendarCopy);
+        //
+        //     })
+        //     .catch(function (error) {
+        //         console.error('Error exporting calendar:', error);
+        //     });
+
+        // pdf.html(calendarHTML, {
+        //     callback: doc => {
+        //         doc.addImage(calendarHTML)
+        //         doc.save(`${fileName}.pdf`);
+        //     },
+        //     x: 15,
+        //     y: 15,
+        //     width: 270,
+        //     windowWidth: 1340,
+        // });
     }
 
     return (
