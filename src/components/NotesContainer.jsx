46f86466919a410de6f61f631e00b1a40a3704d0
Timo_B <timo.buehler01@gmx.de>
2024-03-11 10:09:40 +0100

    Show NotesContainer works

---
+++ src/components/NotesContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotesContainer = () => {
+  return (
+    <div className="fixed top-[65px] right-0 h-screen w-1/4 bg-slate-700 p-4 overflow-y-auto">
+      NotesContainer
+    </div>
+  );
+};
+
+export default NotesContainer;
--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { OutlinedButton } from "./OutlinedButton";
 import { FilledButton } from "./FilledButton";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@nextui-org/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NotesContainer from "./NotesContainer";
 
 export default function PageContainer({
   title,
@@ -19,6 +21,7 @@ export default function PageContainer({
 }) {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [showNotes, setShowNotes] = useState(false);
 
   return (
     <div className={"lg:m-10 m-5 flex flex-col gap-10"}>
@@ -60,9 +63,23 @@ export default function PageContainer({
       <div className={`flex gap-5 ${row ? "flex-row" : "flex-col"}`}>
         {children}
       </div>
+      {showNotes && <NotesContainer />}
       <div className="fixed bottom-5 right-5">
-        <Button isIconOnly color="primary" className="rounded" size="lg">
-          <FontAwesomeIcon icon={"message"} />
+        <Button
+          isIconOnly
+          color="primary"
+          className="rounded"
+          size="lg"
+          onClick={() => {
+            setShowNotes(!showNotes);
+            console.log("ShowNotes: ", showNotes);
+          }}
+        >
+          {showNotes ? (
+            <FontAwesomeIcon icon={"xmark"} />
+          ) : (
+            <FontAwesomeIcon icon={"message"} />
+          )}
         </Button>
       </div>
     </div>
