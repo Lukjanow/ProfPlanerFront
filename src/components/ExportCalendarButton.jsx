0c1dd6e24f80151e74e615cecabec4f75a5f5264
Maik Scherer <inf3934@hs-worms.de>
2024-03-12 11:45:26 +0100

    *export timetable as pdf (timetable is now always on one page)

--- src/components/ExportCalendarButton.jsx
+++ src/components/ExportCalendarButton.jsx
@@ -1,15 +1,14 @@
 import {Button} from "@nextui-org/react";
 import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 import domtoimage from "dom-to-image";
 
 export function ExportCalendarButton() {
 
     function handleExport(fileName) {
-
         const calendar = document.querySelector('.rbc-calendar');
         const calendarCopy = calendar.cloneNode(true);
-        const calendarHeight = calendar.offsetHeight * 2;
+        const calendarHeight = calendar.offsetHeight;
+        const calendarWidth = calendar.offsetWidth;
         calendarCopy.style.height = `${calendarHeight}px`;
 
         const hiddenElements = calendarCopy.querySelectorAll(".hidden");
@@ -23,39 +22,28 @@ export function ExportCalendarButton() {
         const parentElement = calendar.parentElement;
         parentElement.appendChild(calendarCopy);
 
-        // domtoimage.toPng(calendarCopy)
-        //     .then(dataUrl => {
-        //         const pdf = new jsPDF('l', 'mm');
-        //
-        //         pdf.addImage(dataUrl, 'PNG', 15, 15, 270, 0);
-        //         pdf.save(`${fileName}.pdf`);
-        //
-        //         parentElement.removeChild(calendarCopy);
-        //
-        //     })
-        //     .catch(function (error) {
-        //         console.error('Error exporting calendar:', error);
-        //     });
-
-
-
-
-
-
-        domtoimage.toPng(calendarCopy)
+        domtoimage.toJpeg(calendarCopy)
             .then(dataUrl => {
-                const pdf = new jsPDF('l', 'mm');
-                const adjustedImageHeight = 500;
-                const totalPages = Math.ceil(adjustedImageHeight / pdf.internal.pageSize.height);
+                const pdf = new jsPDF('p', 'mm');
+                const pageWidth = pdf.internal.pageSize.width;
+                const pageHeight = pdf.internal.pageSize.height;
 
-                for (let page = 1; page <= totalPages; page++) {
-                    const yOffset = (page - 1) * pdf.internal.pageSize.height;
-                    if (page > 1) {
-                        pdf.addPage();
-                    }
-                    pdf.addImage(dataUrl, 'PNG', 15, 15 - yOffset, 270, adjustedImageHeight);
+                const img = new Image();
+                img.src = dataUrl;
+                const imgWidth = calendarWidth;
+                const imgHeight = calendarHeight;
+                const aspectRatio = imgWidth / imgHeight;
+
+                let scaledWidth, scaledHeight;
+                if (aspectRatio > 1) {
+                    scaledWidth = pageWidth;
+                    scaledHeight = pageWidth / aspectRatio;
+                } else {
+                    scaledHeight = pageHeight;
+                    scaledWidth = pageHeight * aspectRatio;
                 }
 
+                pdf.addImage(dataUrl, 'JPEG', 0, 0, scaledWidth, scaledHeight);
                 pdf.save(`${fileName}.pdf`);
                 parentElement.removeChild(calendarCopy);
 
@@ -63,58 +51,6 @@ export function ExportCalendarButton() {
             .catch(function (error) {
                 console.error('Error exporting calendar:', error);
             });
-
-
-
-
-
-
-
-
-
-
-        // domtoimage.toPng(calendarCopy)
-        //     .then(dataUrl => {
-        //
-        //         const pdf = new jsPDF('l', 'mm');
-        //
-        //         const imgData = dataUrl;
-        //         const imgWidth = 210;
-        //         const pageHeight = 295;
-        //         const imgHeight = calendar.offsetHeight * imgWidth / calendar.offsetWidth;
-        //         let heightLeft = imgHeight;
-        //
-        //         let position = 15;
-        //
-        //         pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-        //         heightLeft -= pageHeight;
-        //
-        //         while (heightLeft >= 0) {
-        //             position += heightLeft - imgHeight;
-        //             pdf.addPage();
-        //             pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-        //             heightLeft -= pageHeight;
-        //         }
-        //
-        //         pdf.save(`${fileName}.pdf`);
-        //
-        //         parentElement.removeChild(calendarCopy);
-        //
-        //     })
-        //     .catch(function (error) {
-        //         console.error('Error exporting calendar:', error);
-        //     });
-
-        // pdf.html(calendarHTML, {
-        //     callback: doc => {
-        //         doc.addImage(calendarHTML)
-        //         doc.save(`${fileName}.pdf`);
-        //     },
-        //     x: 15,
-        //     y: 15,
-        //     width: 270,
-        //     windowWidth: 1340,
-        // });
     }
 
     return (
