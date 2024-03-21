8f20f17a5e3cfde80b5b2e6dc98ad322199aa230
Yannick Stock <inf3896@hs-worms.de>
2024-03-21 11:07:54 +0100

    add attribute to calendar Model

--- src/models/calendarModel.js
+++ src/models/calendarModel.js
@@ -1,13 +1,15 @@
 export class CalendarModel {
-    constructor(name, calendarEntryIdList) {
+    constructor(name, calendarEntryIdList, frequency) {
         this.name = name;
         this.entries = calendarEntryIdList;
+        this.frequency = frequency
     }
 
     setJsonObj(jsonObj) {
         this._id = jsonObj._id;
         this.name = jsonObj.name;
         this.entries = jsonObj.entries
+        this.frequency = jsonObj.frequency
         return this;
     }
 }
