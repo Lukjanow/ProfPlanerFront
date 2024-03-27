90e68c93960d4e1ed571d9526e5e169d09d14716
Lorenz Kehl <mail@lorenzkehl.de>
2024-03-27 14:31:54 +0100

    Last TODO comments removed;

--- src/stores/moduleStore.js
+++ src/stores/moduleStore.js
@@ -1,4 +1,4 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import {
     addModule,
     deleteModule,
@@ -11,7 +11,6 @@ import {
     updateModule
 } from "../services/moduleService.js";
 
-// TODO: unused, should be removed!
 export const useModuleStore = create(
     (set, get) => ({
         moduleList: [],
@@ -20,55 +19,57 @@ export const useModuleStore = create(
         },
         refreshModuleList: async () => {
             const oldModuleList = get().moduleList;
-            const {data} = await getAllModules();
+            const { data } = await getAllModules();
             const updatedModuleList = data.map(newModule => {
                 const oldModule = oldModuleList.find(m => m.id === newModule.id);
                 return newModule ? newModule : oldModule;
             });
-            set(() => ({moduleList: updatedModuleList}));
+            set(() => ({ moduleList: updatedModuleList }));
         },
-        getAllBasicDataModules: async() => {
-            const {data} = await getAllBasicDataModules();
+        getAllBasicDataModules: async () => {
+            const { data } = await getAllBasicDataModules();
             return data;
         },
         getModulesByModuleId: async (moduleId) => {
-            const {data} = await getModulesByModuleId(moduleId);
+            const { data } = await getModulesByModuleId(moduleId);
             return data;
         },
         getModuleById: async (id) => {
-            const {data} = await getModuleById(id);
+            const { data } = await getModuleById(id);
             return data;
         },
         getSelectedModules: async () => {
-            const {data} = await getSelectedModules();
+            const { data } = await getSelectedModules();
             return data;
         },
         getUnselectedModules: async () => {
-            const {data} = await getUnselectedModules();
+            const { data } = await getUnselectedModules();
             return data;
         },
         getAllModulesByDozentId: async (dozentId) => {
-            const {data} = await getAllModulesByDozentId(dozentId);
+            const { data } = await getAllModulesByDozentId(dozentId);
             return data;
         },
         getAllModulesByStudySemesterId: async (studySemesterId) => {
-            const {data} = await getAllModulesByStudySemesterId(studySemesterId);
+            const { data } = await getAllModulesByStudySemesterId(studySemesterId);
             return data;
         },
         addModule: async (moduleModel) => {
-            const {data} = await addModule(moduleModel);
+            const { data } = await addModule(moduleModel);
             await get().refreshModuleList();
             return data;
         },
-        updateModule: async (id, {moduleId, name, code, dozentIdList, roomIdList, studySemesterIdList, duration, approximateAttendance, need, typeList,
-            frequency, selected, color, note, groups}) => {
-            const {data} = await updateModule(id, {moduleId, name, code, dozentIdList, roomIdList, studySemesterIdList, duration, approximateAttendance, need, typeList,
-                frequency, selected, color, note, groups});
+        updateModule: async (id, { moduleId, name, code, dozentIdList, roomIdList, studySemesterIdList, duration, approximateAttendance, need, typeList,
+            frequency, selected, color, note, groups }) => {
+            const { data } = await updateModule(id, {
+                moduleId, name, code, dozentIdList, roomIdList, studySemesterIdList, duration, approximateAttendance, need, typeList,
+                frequency, selected, color, note, groups
+            });
             await get().refreshModuleList();
             return data;
         },
         deleteModule: async (id) => {
-            const {data} = await deleteModule(id);
+            const { data } = await deleteModule(id);
             await get().refreshModuleList();
             return data;
         },
